f68e22dc5ee64305f7f567c7c349c5d0f965dc79
Kate Kim <kay.k.projects@gmail.com>
2022-03-12 19:26:50 +1000

    css updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer>
             <a href="mailto:example@email.email" className="btn btn-secondary btn-sm mb-3">Contact me</a>
             <br></br>
-            <small className="mb-2 d-block text-secondary">Webpage by Kate Kim - <a href="https://github.com/projectkatk/travel-bucket-list-page" rel="noreferrer" target="_blank">Open Sourced</a></small>
+            <small className="mb-2 d-block text-secondary">Travel List by Kate Kim - <a href="https://github.com/projectkatk/travel-bucket-list-page" rel="noreferrer" target="_blank">Github</a></small>
         </footer>
     )
 }
