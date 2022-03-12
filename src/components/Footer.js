aea7172c6bd4840dd8d0b0592a3563ecb6117024
Kate Kim <kay.k.projects@gmail.com>
2022-03-12 19:30:14 +1000

    contact link updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <a href="mailto:example@email.email" className="btn btn-secondary btn-sm mb-3">Contact me</a>
+            <a href="mailto:example@email.email" className="btn btn-secondary btn-sm mb-3">Go back to Projects</a>
             <br></br>
             <small className="mb-2 d-block text-secondary">Travel List by Kate Kim - <a href="https://github.com/projectkatk/travel-bucket-list-page" rel="noreferrer" target="_blank">Github</a></small>
         </footer>
